2f39948dfd2083523f1361b6551c6100ac1e3323
contessa <tessa.fallon@gmail.com>
2015-07-05 20:34:19 -0400

    week 1 sql assignment

---
+++ sql/20150705_week1.sql
@@ -0,0 +1,9 @@
+﻿select COUNT(flight) from flights;
+
+select Count(flight),carrier from flights group by carrier;
+
+select Count(flight),carrier from flights group by carrier order by count desc;
+
+select Count(flight),carrier from flights group by carrier order by count desc limit 5;
+
+select Count(flight),carrier from flights where distance > 1000 group by carrier order by count desc limit 5;
